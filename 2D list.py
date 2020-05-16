df67f64e928a605b7946bab4f473eae690f42110
Enio Fernandes <EN10@users.noreply.github.com>
2020-05-16 10:12:05 +0100

    Create 2D list.py

---
+++ 2D list.py
@@ -0,0 +1,12 @@
+calendar = []
+
+date = '01/01/01'
+time = '10:10'
+
+for x in range(3):
+    event = []
+    event.append(date)
+    event.append(time)
+    calendar.append(event)
+
+print(calendar)
