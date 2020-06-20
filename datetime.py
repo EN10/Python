508adba76870ca95ed7aa80d5cb54506cf21a671
Enio Fernandes <EN10@users.noreply.github.com>
2020-06-20 14:32:02 +0100

    cleanup

--- datetime.py
+++ datetime.py
@@ -4,13 +4,13 @@ def get_date_time():
     while True:
         date = input("Input date dd/mm/yyyy ")
         time = input("Input time hh:mm ")
-        day = date[:2]
-        month = date[3:5]
-        year = date[6:10]
-        hour = time[:2]
-        minute = time[3:5]
+        day = int(date[:2])
+        month = int(date[3:5])
+        year = int(date[6:10])
+        hour = int(time[:2])
+        minute = int(time[3:5])
         try:
-            dt = datetime.datetime(int(year),int(month),int(day),int(hour),int(minute))
+            dt = datetime.datetime(year,month,day,hour,minute)
             return dt
         except ValueError as e:
             print(e)
