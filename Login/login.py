f8ed2cdaaa96de8865f0ef14f43da72e0deddc66
Enio Fernandes <EN10@users.noreply.github.com>
2020-06-20 14:14:02 +0100

    Create login.py

---
+++ Login/login.py
@@ -0,0 +1,7 @@
+print('welcome to the login system')
+password = input('please enter your password: ')
+
+if password == '1234':
+    print('welcome to the system')
+else:
+    print('ERROR: password not recognised')
