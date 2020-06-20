05799a4aa91ff1b7fb9a33d169c5fef18dc071d8
Enio Fernandes <EN10@users.noreply.github.com>
2020-06-20 14:15:46 +0100

    Create loginWhile.py

---
+++ Login/loginWhile.py
@@ -0,0 +1,9 @@
+print('Welcome to the login system')
+
+while(True):
+    password = input('Please enter your password: ')
+    if password == '1234':
+        print('Welcome to the system')
+        break
+    else:
+        print('ERROR: password not recognised')
